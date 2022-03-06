e0959a11f12fc49a0994371a88be94032c871386
formegusto <formegusto@gmail.com>
2022-03-06 16:19:49 +0900

    update fe : webpack config okay

--- session-cert-fe/src/App.tsx
+++ session-cert-fe/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import { publicEncrypt } from "crypto-browserify";
 import { Buffer } from "buffer";
 
 function App() {
   React.useEffect(() => {
     console.log(Buffer.alloc(12, 0));
+    console.log(publicEncrypt);
   }, []);
 
   return <></>;
