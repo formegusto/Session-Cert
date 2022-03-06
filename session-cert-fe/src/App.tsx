96f6b8352596dc662831810a75ade27e4f4eb0c7
formegusto <formegusto@gmail.com>
2022-03-06 16:14:04 +0900

    before eject

--- session-cert-fe/src/App.tsx
+++ session-cert-fe/src/App.tsx
@@ -1,4 +1,12 @@
+import React from "react";
+
+import { Buffer } from "buffer";
+
 function App() {
+  React.useEffect(() => {
+    console.log(Buffer.alloc(12, 0));
+  }, []);
+
   return <></>;
 }
 
